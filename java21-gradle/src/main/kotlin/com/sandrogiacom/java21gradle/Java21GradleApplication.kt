b335920a75972c276d45757078a92d84ba8a9f62
sandro <sandrogiacom@gmail.com>
2025-09-14 19:38:37 -0300

    create salesperson crud

--- java21-gradle/src/main/kotlin/com/sandrogiacom/java21gradle/Java21GradleApplication.kt
+++ java21-gradle/src/main/kotlin/com/sandrogiacom/java21gradle/Java21GradleApplication.kt
@@ -2,8 +2,10 @@ package com.sandrogiacom.java21gradle
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class Java21GradleApplication
 
 fun main(args: Array<String>) {
