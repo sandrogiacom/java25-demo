5afb8931cab1e30a28dd089eecebcd186f4cb33d
sandro <sandrogiacom@gmail.com>
2025-09-14 18:38:58 -0300

    initialize Java 21 Gradle project with Spring Boot and Kotlin support

---
+++ java21-gradle/src/main/kotlin/com/sandrogiacom/java21gradle/Java21GradleApplication.kt
@@ -0,0 +1,11 @@
+package com.sandrogiacom.java21gradle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Java21GradleApplication
+
+fun main(args: Array<String>) {
+    runApplication<Java21GradleApplication>(*args)
+}
---
+++ java21-gradle/src/test/kotlin/com/sandrogiacom/java21gradle/Java21GradleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sandrogiacom.java21gradle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Java21GradleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
